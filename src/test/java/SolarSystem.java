017fe1b16b062f2cccdd94624f35c2ce2402e5ad
clddcloud <artem.maximov@gmail.com>
2024-11-17 15:54:36 +0100

    feature sun added

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -6,5 +6,10 @@ public class SolarSystem {
         BB.Stars="10";
         BB.Sun="1";
         System.out.println("Solar system is ready");
+
+        FeatureSun Xros= new FeatureSun();
+        Xros.temp="21312kj";
+        Xros.color="green";
+        Xros.radius="2312321miles";
     }
 }
